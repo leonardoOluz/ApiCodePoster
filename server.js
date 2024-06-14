2f2d6318bb7b4490d47bac8d0eebef4ebccf2548
Leonardo Luz <leonardoluz10@hotmail.com>
2024-06-13 21:38:43 -0300

    Inicializando projeto

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+import 'dotenv/config';
 import express from 'express';
 const app = express();
 const port = 3000;
+import dbConnect from './src/config/dbConnect.js';
+const conexao = await dbConnect();
+
+conexao.on('error', (error) => {
+    console.log('Erro de conexão', error);
+});
+
+conexao.once('open', () => {
+    console.log('conectado com sucesso !');
+})
 
 app.get('/', (req, res, next) => {
     res.status(200).send({ message: "Hello world" })
---
+++ src/config/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const user = process.env.DB_USER
+const password = process.env.DB_PASS
+
+const uri = `mongodb+srv://${user}:${password}@clusterleoluz.zoaawnu.mongodb.net/db_text-edition?retryWrites=true&w=majority&appName=ClusterLeoLuz`
+
+const dbConnection = async () => {
+    mongoose.connect(uri)
+    return mongoose.connection;
+}
+
+export default dbConnection;

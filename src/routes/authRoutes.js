28358fcb9b2195ab00fee21c11ca90f8c07cfdef
Leonardo Luz <leonardoluz10@hotmail.com>
2024-06-24 11:50:57 -0300

    Teste

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,7 +6,7 @@ const authController = new AuthController();
 const router = Router();
 
 router
-  .post('/usuario/sign-up', (req, res, next) => authController.signUp(req, res, next))
+  .post('/usuario/sign-up', (req, res, next) => authController.signUp(req, res, next));
   .post('/user/login', (req, res, next) => authController.login(req, res, next));
 
 export default router;

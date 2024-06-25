f91d127382466810b3df7cedd50a2fa234ad24e7
Leonardo Luz <leonardoluz10@hotmail.com>
2024-06-24 21:35:01 -0300

    feat: :construction_worker: Criando primeiro teste com jest e incluindo arquivo de CI para testes.

---
+++ src/test/config/dbConfig.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+import mongoose from 'mongoose';
+import { describe, expect, it } from '@jest/globals';
+import 'dotenv/config';
+
+describe('Testando a conexão com o MongoDB', () => {
+  it('Tenta se conectar ao MongoDB e verifica se a conexão é bem-sucedida', async () => {
+    try {
+      await mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true });
+      expect(true).toBeTruthy(); // Verifica se a conexão foi bem-sucedida
+    } catch (error) {
+      console.error(error);
+      expect(false).toBeTruthy(); // Acontece caso haja um erro na conexão
+    }
+  });
+});
